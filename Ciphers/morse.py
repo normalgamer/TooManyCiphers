eae6409d0daaab4f7186dfb2de1c87eebcc1f5fc
normalgamer <jon.iza.2004@gmail.com>
2021-01-16 16:49:05 +0100

    Code cleanup

--- Ciphers/morse.py
+++ Ciphers/morse.py
@@ -1,62 +1,68 @@
 import os
 
-text        = "Hello World! Eooo"
-outputText  = ""
 
-characters          = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']
-morseChars          = ['.-','-...','-.-.','-..','.','..-.','--.','....','..','.---','-.-','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-','...-','.--','-..-','-.--','--..','-----','.----','..---','...--','....-','.....','-....','--...','---..','----.']
-specialCharacters   = [' ','.',',','?','\'','!','/','(',')','&',':',';','=','+','-','_','"','$','@']
-specialMorse        = ['/','.-.-.-','--..--','..--..','.----.','-.-.--','-..-.','-.--.','-.--.-','.-...','---...','-.-.-.','-...-','.-.-.','-....-','..--.-','.-..-.','...-..-','.--.-.']
+characters          = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                       'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                       'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6',
+                       '7', '8', '9']
+morseChars          = ['.-', '-...', '-.-.',  '-..', '.', '..-.', '--.',
+                       '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
+                       '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--',
+                       '-..-', '-.--', '--..', '-----', '.----', '..---',
+                       '...--', '....-', '.....', '-....', '--...', '---..',
+                       '----.']
+specialCharacters   = [' ', '.', ',', '?', '\'', '!', '/', '(', ')', '&', ':',
+                       ';', '=', '+', '-', '_', '"', '$', '@']
+specialMorse        = ['/', '.-.-.-', '--..--', '..--..', '.----.', '-.-.--',
+                       '-..-.', '-.--.', '-.--.-', '.-...', '---...', '-.-.-.',
+                       '-...-', '.-.-.', '-....-', '..--.-', '.-..-.',
+                       '...-..-', '.--.-.']
 
-def toMorse(inputText):
 
-    global outputText
+def encodeMorse(inputText):
+    outputText = ""
     inputText = inputText.lower()
-    
+
     for char in inputText:
         if char in specialCharacters:
             x = specialCharacters.index(char)
-            outputText=outputText+specialMorse[x]+" "
-            
+            outputText = outputText + specialMorse[x] + " "
+
         elif char in characters:
             x = characters.index(char)
-            outputText=outputText+morseChars[x]+" "
+            outputText = outputText + morseChars[x] + " "
         else:
             pass
-        
 
-def decode(inputText):
-    global outputText
-    
+    return outputText
+
+
+def decodeMorse(inputText):
+    outputText = ""
+
     words = inputText.split('   ')
-    
+
     for word in words:
-        l = word.split(' ')
-        for char in l:
+        chars = word.split(' ')
+        for char in chars:
             if char in specialMorse:
                 x = specialMorse.index(char)
-                outputText=outputText+specialCharacters[x]
-            
+                outputText = outputText + specialCharacters[x]
+
             elif char in morseChars:
                 x = morseChars.index(char)
-                outputText=outputText+characters[x]
+                outputText = outputText + characters[x]
             else:
                 pass
-        outputText = outputText+" "
+        outputText = outputText + " "
+
+    return outputText
 
 
+message = "The quick brown fox jumps over the lazy dog"
 
-print("*** MORSE ENCODER/DECODER ***")
-toMorse(text)
+encrypted = encodeMorse(message)
+print(encrypted)
 
-print(outputText)
-
-input()
-
-morseText = outputText
-outputText = ""
-
-decode(morseText)
-print(outputText)
-
-input()
+decrypted = decodeMorse(encrypted)
+print(decrypted)
